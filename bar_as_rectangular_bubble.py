74c211091a59c08b6136a24e0a15176e640284f1
Oli <oli@tigmus.com>
2018-03-05 08:20:21 +0000

    have achieved view initially sketched by Mirit

--- bar_as_rectangular_bubble.py
+++ bar_as_rectangular_bubble.py
@@ -1,17 +1,44 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import random
 
-fig, ax = plt.subplots(nrows=3, ncols=3, sharex=True, sharey=True, figsize=(14, 8))
+fig, ax = plt.subplots(nrows=1, ncols=7, sharex=True, sharey=True, figsize=(14, 8))
+# sns.set()
 
-fig.text(0.5, 0.04, 'number of people', ha='center')
-fig.text(0.04, 0.5, 'number of visits', va='center', rotation='vertical')
+fig.text(0.5, 0.04, 'Number of people (represented by area of box)', ha='center')
+fig.text(0.04, 0.5, 'Number of visits (1-10)', va='center', rotation='vertical')
 
-days = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
-    
+days = ['Mon',
+		'Tue',
+		'Wed','Thu','Fri','Sat','Sun'
+		]
+m = 25
+
 for day in days:
-    plt.subplot(1,7,days.index(day)+1)
-    plt.barh(range(36),random.sample(range(1, 100), 36))
-    plt.xlabel(day)
-    
+	results = range(10)
+	data = []
+	for result in results:
+		a = random.choice(range(10))
+		data.append([m-a,m,m+a])
+	ax = plt.subplot(1,7,days.index(day)+1)
+	medianprops = dict(linestyle=None, linewidth=0)
+	plt.boxplot(data, notch=0,
+				sym='',
+				vert=0,
+				whis=0,
+				medianprops=medianprops,
+				patch_artist=True,
+				boxprops=dict(facecolor='blue', color='white'))
+	plt.xlabel(day)
+	ax.spines['right'].set_visible(False)
+	ax.spines['top'].set_visible(False)
+	ax.spines['bottom'].set_visible(False)
+	ax.spines['left'].set_visible(False)
+	ax.yaxis.set_ticks([0])
+	ax.xaxis.set_ticks([0])
+#	ax.yaxis.set_ticks([10,20,30,40])
+	ax.xaxis.set_ticklabels('')
+	plt.setp(ax.get_yticklabels(), visible=False)
+
 plt.subplots_adjust(wspace=1)
 plt.show()
